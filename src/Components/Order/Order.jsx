adfeed642879204b88118ccfcc7daeb2e88c0e34
wooju <wooju57@google.com>
2023-09-20 15:25:37 +0900

    test5

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -31,7 +31,7 @@ function Order() {
   const moveError = useNavigate();
   //메뉴 api 연동
   useEffect(() => {
-    // window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
     async function fetchData() {
       let menuRes = await menu();
       //accessToken 없이 접속하는 경우
