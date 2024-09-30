fd98be7fbb5dd45ccc5d230d00cbe6ec3033d36b
rudeore-098 <rudeore0928@gmail.com>
2024-09-30 14:46:16 +0900

    세부 디테일 조정

--- src/Components/Mainpage/Mainpage.jsx
+++ src/Components/Mainpage/Mainpage.jsx
@@ -42,7 +42,7 @@ function Mainpage({ tableNo }) {
     <div className="order-app">
       <div className="top-bar">
         <div className="logo-container">
-          <img src="/logo2024.png" className="logo"></img>
+          <img src="/logoit2024.png" className="logo"></img>
           {/* <div className='logo-title'>2023 IT대학 대동제 주점</div> */}
         </div>
       </div>
